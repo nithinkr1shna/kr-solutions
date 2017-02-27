81a5c7a24caf13c8bd1b86e8681599e914d1e261
nitkna <nithin.krishna@outlook.com>
2017-02-27 15:17:39 +0530

    patter matching from right

--- chapter4/4.1.c
+++ chapter4/4.1.c
@@ -1,3 +1,6 @@
+
+
+
 #include<stdio.h>
 #include<string.h>
 #define MAXLENGTH 1000
@@ -7,14 +10,15 @@ int strindex(char s[], char t[]);
 
 int main(){
 
-  int length, found =0;
+  int length, found =0,i;
   char string[MAXLENGTH], pattern[MAXLENGTH];
-  printf("Enter the pattern to search");
+  printf("Enter the pattern to search\n");
   mgetline(pattern, MAXLENGTH);
+  printf("Enter the string to check\n");
   while (mgetline(string, MAXLENGTH) >0){
-    if(strindex(string, pattern) >= 0){
-      printf("%s",string);
-      found++;
+    if((i=strindex(string, pattern)) >= 0){
+      printf("%s\n",string);
+      printf("the position of occurance is\t%d\n",i-1);
     }
     return found;
     
@@ -37,14 +41,11 @@ int mgetline(char string[], int limit){
 int strindex(char s[], char t[]){
   
   int i, j, k;
-  int line_len = strlen(s);
-  int pattern_len = strlen(t);
+  int line_len = strlen(s)-1;
+  int pattern_len = strlen(t)-1;
   
   for (i = line_len; i>=0; i--) {
-    printf("%d\t",i);
     for (j=i, k=pattern_len; ((k>=0) && (s[j]==t[k]));j--,k--){
-      printf("insidej %d\t",j);
-      printf("insidek %d\t",k);
       
       if (k==0){
 	
@@ -56,6 +57,3 @@ int strindex(char s[], char t[]){
 return -1;
  
 }
-
-
-
