d16e4e608926f90fa68a84b4fbd7c387dd43d2dd
nitkna <nithin.krishna@outlook.com>
2017-03-01 16:43:07 +0530

    single character push back with EOF

---
+++ chapter4/4.9.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<string.h>
+#define MAXLEN 50
+#define MAXBUF 50
+
+int mgetline(char s[], int lim);
+void ungetch(int c);
+int getch(void);
+void ungets(char s[]);
+
+int  buf[MAXLEN];
+int bufp;
+int main(){
+  char string[MAXLEN];
+  int c,k=0;
+  mgetline(string, MAXLEN);
+  ungets(string);
+  
+  
+  while((c=getch())!=EOF){
+    putchar(c);
+    printf("\n");
+  }
+
+ 
+  for(k=0;k<MAXLEN;k++){
+    buf[k]=0;
+  }
+}
+
+
+int mgetline(char s[], int lim){
+  
+  int i, c;
+  for(i=0;i<lim-1 && (c=getchar())!=EOF && c != '\n';i++){
+      s[i]=c;
+   }
+   if(c=='\n')
+     s[i++]=c;
+      
+   s[i]='\0';
+  
+
+  return i;
+
+}
+
+
+
+void ungetch(int c)
+{
+    if(bufp >= MAXBUF)
+        printf("ungetch: too many characters\n");
+    else{
+        buf[bufp++]=c;
+
+    }
+}
+
+
+int getch(void)
+{
+    return (bufp == 0)?buf[--bufp]:getchar();
+}
+
+void ungets(char s[])
+{
+  int i,k;
+    
+  i = 0;
+  printf("ungets %c", s[i]);
+  ungetch(s[i]);
+}
