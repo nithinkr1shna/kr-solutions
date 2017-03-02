79dab62af260260507ff4e2df664e9a2702ec41e
nitkna <nithin.krishna@outlook.com>
2017-03-02 13:39:27 +0530

    string revesing with recursion

---
+++ chapter4/4.13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void reverse();
+
+int main()
+{
+    printf("Enter a sentence: ");
+    reverse();
+    printf("\n");
+    return 0;
+}
+
+void reverse()
+{
+    char c;
+    scanf("%c", &c);
+
+    if( c != '\n')
+    {
+        reverse();
+        printf("%c",c);
+    }
+}
