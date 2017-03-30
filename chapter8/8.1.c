37bb96dad7007be0dce7d07a0b46ad299ca68609
nitkna <nithin.krishna@outlook.com>
2017-03-30 12:36:11 +0530

    use of read and write to attain cat functionality

---
+++ chapter8/8.1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<sys/file.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#define BUFSIZE 5000 
+
+int main(int argc, char **argv){
+
+  int fd,i=0,n;
+  char buf[BUFSIZE];
+  if(argc == 1){
+    printf("Enter file names as command line arguments\n");
+    exit(0);
+  }else{
+    for(;argc-- > 0;i++){
+
+      fd =open(argv[i],O_RDONLY,0);
+      n=read(fd,buf,BUFSIZE);
+      if(write(1,buf,n) !=n)
+	printf("Write Error\n");
+    }
+  }
+
+}
